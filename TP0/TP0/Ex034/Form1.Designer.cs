4e3b6533733862deab46d9ff7b24cb3ff2979d8f
yasser <y.laachir.m@gmail.com>
2023-10-25 22:29:00 +0100

    client w/ struct code added

--- TP0/TP0/Ex034/Form1.Designer.cs
+++ TP0/TP0/Ex034/Form1.Designer.cs
@@ -28,12 +28,112 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            lblPrenom = new Label();
+            lblNom = new Label();
+            lblDdn = new Label();
+            txtPrenom = new TextBox();
+            txtNom = new TextBox();
+            txtDdn = new TextBox();
+            btnAjouterClient = new Button();
+            btnRechercher = new Button();
+            SuspendLayout();
+            // 
+            // lblPrenom
+            // 
+            lblPrenom.AutoSize = true;
+            lblPrenom.Location = new Point(12, 18);
+            lblPrenom.Name = "lblPrenom";
+            lblPrenom.Size = new Size(49, 15);
+            lblPrenom.TabIndex = 0;
+            lblPrenom.Text = "Prenom";
+            // 
+            // lblNom
+            // 
+            lblNom.AutoSize = true;
+            lblNom.Location = new Point(12, 67);
+            lblNom.Name = "lblNom";
+            lblNom.Size = new Size(34, 15);
+            lblNom.TabIndex = 1;
+            lblNom.Text = "Nom";
+            // 
+            // lblDdn
+            // 
+            lblDdn.AutoSize = true;
+            lblDdn.Location = new Point(12, 114);
+            lblDdn.Name = "lblDdn";
+            lblDdn.Size = new Size(101, 15);
+            lblDdn.TabIndex = 2;
+            lblDdn.Text = "Date de naissance";
+            // 
+            // txtPrenom
+            // 
+            txtPrenom.Location = new Point(119, 15);
+            txtPrenom.Name = "txtPrenom";
+            txtPrenom.Size = new Size(139, 23);
+            txtPrenom.TabIndex = 3;
+            // 
+            // txtNom
+            // 
+            txtNom.Location = new Point(119, 64);
+            txtNom.Name = "txtNom";
+            txtNom.Size = new Size(139, 23);
+            txtNom.TabIndex = 4;
+            // 
+            // txtDdn
+            // 
+            txtDdn.Location = new Point(119, 111);
+            txtDdn.Name = "txtDdn";
+            txtDdn.Size = new Size(139, 23);
+            txtDdn.TabIndex = 5;
+            // 
+            // btnAjouterClient
+            // 
+            btnAjouterClient.Location = new Point(12, 161);
+            btnAjouterClient.Name = "btnAjouterClient";
+            btnAjouterClient.Size = new Size(104, 27);
+            btnAjouterClient.TabIndex = 6;
+            btnAjouterClient.Text = "Ajouter client";
+            btnAjouterClient.UseVisualStyleBackColor = true;
+            btnAjouterClient.Click += btnAjouterClient_Click;
+            // 
+            // btnRechercher
+            // 
+            btnRechercher.Location = new Point(154, 161);
+            btnRechercher.Name = "btnRechercher";
+            btnRechercher.Size = new Size(104, 27);
+            btnRechercher.TabIndex = 7;
+            btnRechercher.Text = "Rechercher";
+            btnRechercher.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(274, 206);
+            Controls.Add(btnRechercher);
+            Controls.Add(btnAjouterClient);
+            Controls.Add(txtDdn);
+            Controls.Add(txtNom);
+            Controls.Add(txtPrenom);
+            Controls.Add(lblDdn);
+            Controls.Add(lblNom);
+            Controls.Add(lblPrenom);
+            Name = "Form1";
+            Text = "Client";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label lblPrenom;
+        private Label lblNom;
+        private Label lblDdn;
+        private TextBox txtPrenom;
+        private TextBox txtNom;
+        private TextBox txtDdn;
+        private Button btnAjouterClient;
+        private Button btnRechercher;
     }
 }
--- TP0/TP0/Ex034/Form1.cs
+++ TP0/TP0/Ex034/Form1.cs
@@ -2,9 +2,57 @@ namespace Ex034
 {
     public partial class Form1 : Form
     {
+        private struct Client
+        {
+            public int Id;
+            public String Prenom;
+            public String Nom;
+            public DateTime DateDeNaissance;
+        }
+
+        private List<Client> tabClient = new List<Client>();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private Client createClient(String Nom, String Prenom, String DdN)
+        {
+            Client newClient = new Client();
+            newClient.Id = tabClient.Count;
+            newClient.Prenom = Prenom;
+            newClient.Nom = Nom;
+            newClient.DateDeNaissance = DateTime.Parse(DdN);
+            return newClient;
+        }
+
+        private void addClient(Client client)
+        {
+            tabClient.Add(client);
+            MessageBox.Show(
+                txtNom.Text + " " + txtPrenom.Text + " à été ajouté avec success",
+                "new Slave Added",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information
+            );
+        }
+
+        private void clearTextBoxes()
+        {
+            txtPrenom.Text = txtNom.Text = txtDdn.Text = "";
+        }
+
+        private void btnAjouterClient_Click(object sender, EventArgs e)
+        {
+            Client newClient = createClient(txtPrenom.Text, txtNom.Text, txtDdn.Text);
+            addClient(newClient);
+            clearTextBoxes();          
         }
     }
 }
